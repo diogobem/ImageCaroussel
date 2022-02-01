3835b6d80992398b31fbac0964f798b6c86f30a4
diogobem <diogobem@yahoo.com.br>
2022-02-01 08:59:37 -0300

    final version

--- carousel.js
+++ carousel.js
@@ -7,7 +7,7 @@ slideLeft.addEventListener('click', side_slide)
 slideRight.addEventListener('click', side_slide)
 
 addBtnSliders(img)
-
+//autoslide(5000)
 
 function addBtnSliders(img) {
 
@@ -16,11 +16,11 @@ function addBtnSliders(img) {
         let btnSliderDiv = carousel.querySelector('.btn-sliders')
         const span = document.createElement('span')
         span.classList.add('sliderBox')
-        span.dataset.index = i+1;
+        span.dataset.index = i + 1;
         btnSliderDiv.append(span)
         span.addEventListener('click', btm_slider)
-        if (i==0){
-            span.style.background = 'white' 
+        if (i == 0) {
+            span.style.background = 'white'
         }
     }
 
@@ -28,34 +28,43 @@ function addBtnSliders(img) {
 
 function btm_slider(e) {
     let carousel = e.target.parentElement.parentElement
-    indexValue=e.target.dataset.index
-    showImage( carousel)
+    indexValue = e.target.dataset.index
+    showImage(carousel)
 }
 
 function side_slide(e) {
 
     let carousel = e.target.parentElement.parentElement.parentElement
-    const carouselImg= carousel.querySelectorAll('.carousel_img')
+    const carouselImg = carousel.querySelectorAll('.carousel_img')
     const direction = e.target.dataset.slider;
     if (direction === 'right') {
         indexValue++
-        indexValue > carouselImg.length?indexValue = 1:indexValue
+        indexValue > carouselImg.length ? indexValue = 1 : indexValue
     } else {
         indexValue--
-        indexValue == 0?indexValue = carouselImg.length:indexValue
+        indexValue == 0 ? indexValue = carouselImg.length : indexValue
     }
     showImage(carousel)
 }
 
 function showImage(carousel) {
     const slider = carousel.querySelectorAll('.btn-sliders span')
-    const carouselImg= carousel.querySelectorAll('.carousel_img')
-     console.log(indexValue)
-   for (i = 0; i < carouselImg.length; i++) {
-    carouselImg[i].style.display = 'none';
+    const carouselImg = carousel.querySelectorAll('.carousel_img')
+    for (i = 0; i < carouselImg.length; i++) {
+        carouselImg[i].style.display = 'none';
         slider[i].style.background = 'none'
     }
 
     carouselImg[indexValue - 1].style.display = 'block'
     slider[indexValue - 1].style.background = 'white'
+}
+
+function autoslide(interval){
+    setInterval(()=>{
+        const carousel = document.querySelector('.carousel')
+        const carouselImg = carousel.querySelectorAll('.carousel_img')
+        indexValue++;
+        indexValue > carouselImg.length ? indexValue = 1 : indexValue
+        showImage(carousel)
+    },interval)
 }
